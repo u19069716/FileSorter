f9e8fb35ff057355646b86fc1bbe5943b1e32651
Alex <u19069716@tuks.co.za>
2021-07-20 15:20:21 +0200

    testing(FileSorter): tested functionality in main.py

--- main.py
+++ main.py
@@ -3,18 +3,26 @@
 ###########
 
 from FileSorter.functions import *
+from FileSorter.classes import *
 
 #################
 # Configuration #
 #################
 
 directories = ['/home/alexwhuman/Downloads/']
+sortingConfiguration = [
+    sortConfig('/home/alexwhuman/Downloads/textFiles',
+               [matchesFileExtensions(['.txt'])]),
+    sortConfig('/home/alexwhuman/Downloads/Unsorted', [sortCriterion()])
+]
 
 ################
 # Main Program #
 ################
 
 for directory in directories:
-    print("Directory: " + directory)
-    for content in getFormattedDirectoryContents(directory):
-        print('\t' + content)
+    directoryPurePaths, nonDirectoryPurePaths = getPurePathsFromDirectory(
+        directory)
+    sortedPurePaths = sortPurePathsOnCriteria(
+        nonDirectoryPurePaths, sortingConfiguration)
+    printFormattedSortedPurePaths(sortedPurePaths)
